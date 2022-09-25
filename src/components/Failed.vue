23ac7b12045e2cea3799eb386986e4bd2d36ff0b
Aaski-M <1041220793@qq.com>
2022-09-25 18:06:39 +0800

    feat: change header menu

---
+++ src/components/Failed.vue
@@ -0,0 +1,36 @@
+<script setup lang="ts">
+import PDFIcon from '../assets/pdf-icon.png'
+interface FailedProps {
+  msg: string
+}
+
+const { msg } = defineProps<FailedProps>()
+</script>
+<template>
+  <div class="error-container">
+    <img class="error-icon" :src="PDFIcon" />
+    <p class="error-text">{{msg}}</p>
+  </div>
+
+</template>
+
+<style lang="scss">
+.error-container {
+  width: 100%;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+
+  .error-icon {
+    width: 50px;
+    height: 50px;
+  }
+
+  .error-text {
+    color: #FF1D1D;
+    font-size: 12px;
+    font-weight: 700;
+  }
+}
+</style>
---
+++ src/components/Header.vue
@@ -0,0 +1,55 @@
+<script lang="ts" setup>
+import NextPageIcon from '../assets/next.svg'
+import PrevPageIcon from '../assets/prev.svg'
+import ScaleBigPageIcon from '../assets/scale-big.svg'
+import ScaleSmallPageIcon from '../assets/scale-small.svg'
+
+interface HeaderProps {
+  total: number
+  current: number
+}
+
+const { total, current } = defineProps<HeaderProps>()
+const emits = defineEmits(['nextPage', 'prevPage'])
+
+const onNextPage = () => {
+  emits('nextPage')
+}
+
+const onPrevPage = () => {
+  emits('prevPage')
+}
+
+const onScaleBigPage = () => {
+
+}
+
+const onScaleSmallPage = () => {
+
+}
+</script>
+
+<template>
+  <div class="pdf-viewer-header">
+    <img :src="PrevPageIcon" @click="onPrevPage" />
+    <span>{{current}} / {{total}}</span>
+    <img :src="NextPageIcon" @click="onNextPage" />
+    <img :src="ScaleBigPageIcon" />
+    <img :src="ScaleSmallPageIcon" />
+  </div>
+</template>
+
+<style lang="scss">
+.pdf-viewer-header {
+  width: 100%;
+  height: 32px;
+  display: flex;
+  align-items: center;
+  background-color: #ccc;
+
+  img {
+    width: 24px;
+    height: 24px;
+  }
+}
+</style>
